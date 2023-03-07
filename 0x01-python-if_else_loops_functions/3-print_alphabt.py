a97db548d22a17ff970fe5a02753764e05509c34
frizydata <ojokof@gmail.com>
2023-03-07 23:34:19 +0100

    alphabet soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 for xters in range(97, 123):
-    if xters != 101 and xters != 113:
-        print("{:c}".format(xters, end=''))
+    if xters == 101 or xters == 113:
+        continue
+    print("{}".format(xters), end='')
