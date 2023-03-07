5ecf35ba1e0696cc53a979462cc0b8b24f319182
frizydata <ojokof@gmail.com>
2023-03-08 00:40:34 +0100

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,4 +4,4 @@ for i in range(0, 100):
     if i == 99:
         print(i)
 else:
-    print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
+    print("{}".format('0' + str(i) if i < 10 else i), end=", ")
