218ce44a9b2934b7277e69f6af4b5aae9e6f4983
frizydata <ojokof@gmail.com>
2023-03-09 21:20:46 +0100

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
 
-count = len(sys.argv) - 1
-if count == 0:
+count_list = len(sys.argv) - 1
+if count_list == 0:
     print("0 arguments.")
-elif count == 1:
+elif count_list == 1:
     print("1 argument:")
 else:
-    print("{} arguments:".format(count))
-for i in range(count):
+    print("{} arguments:".format(count_list))
+for i in range(count_list):
     print("{}: {}".format(i + 1, sys.argv[i + 1]))
